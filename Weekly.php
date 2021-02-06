af73c29aaf5fb94d869fc56d7bff7282bcf988c6
JinSY515 <73789740+JinSY515@users.noreply.github.com>
2021-02-06 23:04:39 +0900

    나만의 다이어리 만들기_1일차
    각 nav를 눌렀을 때 각 장으로 이동하도록 구현

---
+++ Weekly.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/ui.css" type='text/css'>
+    <script type="text/javascript" src="./js/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="./js/ui.js"></script>
+    <title>To do list</title>
+</head>
+<body>
+    <section id="layout">
+        <header>
+            <strong class="logo"><a href="To_do_list.php"><img src="images/diary.png"></a></strong>
+            <li id="main"><a href="To_do_list.php">Main</a></li>
+            <nav>
+                <ul>
+                    <li><a href="Annual.php">Annual</a></li>
+                    <li><a href="Monthly.php">Monthly</a></li>
+                    <li class="on"><a href="Weekly.php">Weekly</a></li>
+                    <li><a href="Daily.php">Daily</a></li>
+                    
+                </ul>
+            </nav>
+        </header>
+        <section id="container">
+            
+        </section>
+
+        <footer>
+            <p>HTML,CSS,JavaScript,PHP,MySQL<br>
+            를 이용한 To do list/Diary 만들기</p>
+            <p>Copyright(c)2021 JinSY515 <br>All Rights Reserved</p>
+        </footer>
+    </section>
+    
+
+</body>
+</html>
